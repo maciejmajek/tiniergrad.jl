1c8d45be9d54bb855c8f3353bec5dbd9b556a249
Maciejeg <maciek.majek2@gmail.com>
2023-05-02 15:51:05 +0200

    Update example script

--- example.jl
+++ example.jl
@@ -21,10 +21,10 @@ function cnn_train_epoch(
         x = Constant(add_dim(trainx[:, :, i]))
         y = Constant(trainy[i, :])
 
-        graph = CNN(x, y, k1, k2, k3, w1, b1, w2, b2)
+        graph = CNN(x, y, k1, k2, k3, k4, w1, b1, w2, b2)
         epoch_loss += train_step!(graph)
         if i % batch_size == 0
-            step!(graph, lr)
+            step!(graph, lr/batch_size)
             zero_grad!(graph)
         end
         set_description(iter, string(@sprintf("Train Loss: %.3f", epoch_loss / i)))
@@ -38,7 +38,7 @@ function cnn_test_epoch(testx::Array{Float64,3}, testy::Array{Float64,2})
     for i in iter
         x = Constant(add_dim(testx[:, :, i]))
         y = Constant(testy[i, :])
-        graph = CNN(x, y, k1, k2, k3, w1, b1, w2, b2)
+        graph = CNN(x, y, k1, k2, k3, k4, w1, b1, w2, b2)
         epoch_loss += test_step(graph)
         zero_grad!(graph)
         set_description(iter, string(@sprintf("Test Loss: %.3f", epoch_loss / i)))
@@ -65,18 +65,20 @@ function CNN(
     k1::Variable,
     k2::Variable,
     k3::Variable,
+    k4::Variable,
     w1::Variable,
     b1::Variable,
     w2::Variable,
     b2::Variable,
 )
     z1 = conv2d(x, k1) |> relu
-    z2 = conv2d(z1, k2) |> relu
-    z3 = conv2d(z2, k3) |> relu |> flatten
-    z4 = dense(z3, w1, b1) |> relu
-    z5 = dense(z4, w2, b2)
+    z2 = conv2d(z1, k2) |> maxpool2d |> relu
+    z3 = conv2d(z2, k3) |> maxpool2d |> relu
+    z4 = conv2d(z3, k4) |> maxpool2d |> flatten
+    z5 = dense(z4, w1, b1) |> relu
+    z6 = dense(z5, w2, b2)
 
-    loss = cross_entropy_loss(z5, y)
+    loss = cross_entropy_loss(z6, y)
     graph = topological_sort(loss)
     return graph
 end
@@ -86,25 +88,27 @@ conv_op = NNlib.conv
 debug = false
 ##
 
-k1 = Variable(create_kernel(1, 4));
-k2 = Variable(create_kernel(4, 8));
-k3 = Variable(create_kernel(8, 16));
-w1 = Variable(kaiming_normal_weights(512, 22 * 22 * 16));
-w2 = Variable(kaiming_normal_weights(10, 512));
-b1 = Variable(initialize_uniform_bias(22 * 22 * 16, 512));
-b2 = Variable(initialize_uniform_bias(512, 10));
+k1 = Variable(create_kernel(1, 16));
+k2 = Variable(create_kernel(16, 32));
+k3 = Variable(create_kernel(32, 32));
+k4 = Variable(create_kernel(32, 64));
+
+w1 = Variable(kaiming_normal_weights(128, 64));
+w2 = Variable(kaiming_normal_weights(10, 128));
+b1 = Variable(initialize_uniform_bias(64, 128));
+b2 = Variable(initialize_uniform_bias(128, 10));
 
 
-for j = 1:10
+for j = 1:5
     println("Epoch $j")
-    perm = randperm(100)
+    perm = randperm(60_000)
     push!(
         train_loss,
         cnn_train_epoch(
             train_x[:, :, perm],
             train_y[perm, :],
-            lr = 2e-3 / (j),
-            batch_size = 32,
+            lr = 4e-3/j,
+            batch_size = 16,
         ),
     )
     push!(test_loss, cnn_test_epoch(test_x, test_y))
